74f7483f7b030a85121f0fc20e08d139d8f31faf
Jaydeep Rangani <ranganijaydeep@yahoo.in>
2020-04-05 08:47:57 +0100

    add enum continent

---
+++ src/patterncombination/Continents.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package patterncombination;
+
+/**
+ *
+ * @author Jaydeep Rangani
+ */
+public enum Continents {
+    Asia,
+    Africa,
+    Antarctica,
+   NorthAmerica,
+   SouthAmerica,
+   Europe,
+   Oceania;
+   
+   
+    
+            
+      
+           
+    
+    
+    
+}
