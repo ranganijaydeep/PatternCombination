13afddfdfdf1c655b61ff476e8a54ed37c09b18d
Jaydeep Rangani <ranganijaydeep@yahoo.in>
2020-04-05 09:50:47 +0100

    MAIN COMMENTED

--- src/patterncombination/PatternCombination.java
+++ src/patterncombination/PatternCombination.java
@@ -15,41 +15,43 @@ public class PatternCombination   {
 
     
     public PatternCombination(){     
-        //int to get nuber for selcection 
+      
      
          
     }
     public static void main(String[] args) {
        // PatternCombination patternCombination = new PatternCombination(); 
          
-        
-      
-       
-        
-      
-      
-      
+       // calling reference from interface 
+       // client dont have direct access to database ..
       CountryINT  data= new CountryDAO();
          
             
-        
+       //saasign array list to country data .. 
       ArrayList<Country> Countries = data.getCountries();
-        
+      
+      //using for loop to get all country detail from database in squencial order.
       for(Country c : Countries){
-           System.out.println(c);
+           System.out.println(c);// printing each record in text 
         }
       
       
-      
-                          
+      // THIS REFERCES TO INTERFACE METHOD TO VIEW COUNTRY BY COUNTRY CODE 
+                               
         Country c = data.SearchCountrybycode("353");
+        // PRINTING COUNTRY DETAIL THAT HAS BEEN MATCH 
         System.out.println(c);
         
+        
+        //THIS IS METHOD REFERENCES FROM INTERFACE TO GET COUNTRY BY NAME
         Country noc = data.SearchCountrybyname("ireland");
+        // PRINTING MATCHING RECORD.
         System.out.println(noc);
        
-          
+          // THIS REFERNCES TO METHOD TO ADD COUNTRY DEATAIL (CELL IN DB)
         Country newentry = new Country( "990","jaydeep", "Asia", "99999","xx");
+        //PREVIUS SETUP OBJECT CREATS SQL SENTENCE  
+        //PRINT ST. GETTING CONFIRMATION OF PROCESS DONE..
         System.out.println(data.insertCountry(newentry));
          
     }
